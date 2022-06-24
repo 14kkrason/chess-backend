a8ad44bbe01cbb81d862a436227300903ca72105
14kkrason <14krzysztof.krason@gmail.com>
2022-06-24 14:25:13 +0200

    test(Auth): added tests for password service

--- src/auth/password.service.spec.ts
+++ src/auth/password.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PasswordService } from './password.service';
+// import * as crypto from 'crypto';
 
 describe('PasswordService', () => {
   let service: PasswordService;
@@ -14,5 +15,16 @@ describe('PasswordService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should be a string', async () => {
+    const pass = await service.generatePassword();
+    expect(typeof pass).toBe('string');
+  });
+
+  it('should be between 28 and 40', async () => {
+    const pass = await service.generatePassword();
+    expect(pass.length).toBeGreaterThanOrEqual(28);
+    expect(pass.length).toBeLessThanOrEqual(40);
   });
 });
